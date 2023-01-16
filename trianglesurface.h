b70771645f4c18bfbf465bb5768e581a5f13ab2d
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-01-16 11:02:52 +0100

    tringle

--- trianglesurface.h
+++ trianglesurface.h
@@ -7,6 +7,14 @@ class TriangleSurface : public VisualObject
 {
 public:
     TriangleSurface();
+    TriangleSurface(std::string filnavn);
+    ~TriangleSurface() override;
+    void readFile(std::string filnavn);
+
+    void init(GLint shader) override;
+    void draw() override;
+
+
 };
 
 #endif // TRIANGLESURFACE_H
