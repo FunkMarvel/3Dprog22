cba5ad1f39543091a521bb2d8e52acb8a1ba4a83
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-01-26 13:50:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QMatrix4x4>
+#include <QVector4D>
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +15,18 @@ int main(int argc, char *argv[])
     //Makes the Qt MainWindow and shows it.
     MainWindow w;
     w.show();
+
+    std::vector<float> AVals{
+        1,1,1,0,
+        5,3,2,0,
+        1,3,2,0,
+        0,0,0,1
+    };
+    QMatrix4x4 A{AVals.data()};
+    QVector4D b{1,2,1,1};
+
+    auto B = A.inverted();
+    qDebug() << B*b;
 
     return a.exec();
 }
