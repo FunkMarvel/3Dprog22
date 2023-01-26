8a7e9627d2c9fd0ffa93122a37166f761ab9bf84
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-01-26 13:51:02 +0100

    started implementing camera

--- camera.cpp
+++ camera.cpp
@@ -4,3 +4,23 @@ Camera::Camera()
 {
 
 }
+
+void Camera::init(GLint pMatrixUniform, GLint vMatrixUniform)
+{
+}
+
+void Camera::perspective(int degrees, double aspect, double nearPlane, double farPlane)
+{
+}
+
+void Camera::lookAt(const QVector3D& eye, const QVector3D& at, const QVector3D& up)
+{
+}
+
+void Camera::update()
+{
+}
+
+void Camera::translate(float dx, float dy, float dz)
+{
+}
--- camera.h
+++ camera.h
@@ -1,16 +1,27 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <QMatrix4x4>
 #include <QOpenGLFunctions_4_1_Core>
-
+#include <qvectornd.h>
 
 
 class Camera : QOpenGLFunctions_4_1_Core
 {
-public:
-    Camera();
-
 private:
+   GLint  _mPMatrixUniform;        //OpenGL reference to the Uniform in the shader program
+   GLint  _mVMatrixUniform;        //OpenGL reference to the Uniform in the shader program
+   QVector3D _mEye;
+   QMatrix4x4 _mPMatrix{};         // denne,
+   QMatrix4x4 mVMatrix{};         // og denne, skal legges inn i kameraklasse
+public:
+   Camera();
+   ~Camera() override = default;
+   void init(GLint pMatrixUniform, GLint vMatrixUniform);
+   void perspective(int degrees, double aspect, double nearPlane, double farPlane);
+   void lookAt(const QVector3D& eye, const QVector3D& at, const QVector3D& up);
+   void update();
+   void translate(float dx, float dy, float dz);
 
 };
 
