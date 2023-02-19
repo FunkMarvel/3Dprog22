c2e853a4a9b32b4063279ad754f8e30e7a537f48
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-02-19 17:21:32 +0100

    Added Eigen lib to project

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QApplication>
 #include <QMatrix4x4>
 #include <QVector4D>
+#include <Eigen/Eigen>
 
 int main(int argc, char* argv[]) {
     //Forces the usage of desktop OpenGL - Qt uses OpenGL ES as default
@@ -14,6 +15,18 @@ int main(int argc, char* argv[]) {
     //Makes the Qt MainWindow and shows it.
     MainWindow w;
     w.show();
+
+    Eigen::Matrix<float, 3, 3> A{
+        {1, 2, 3},
+        {2, 3, 1},
+        {3, 1, 2}
+    };
+
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            qDebug() << A(i,j);
+        }
+    }
 
     return a.exec();
 }
